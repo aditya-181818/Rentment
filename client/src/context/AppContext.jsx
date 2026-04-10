5aa70f89aef17abf0c68614c3a4b7226f2e0cd3c
aditya-181818 <adityamaurya05393@gmail.com>
2026-04-10 10:23:07 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import toast from 'react-hot-toast'
 import { useNavigate } from "react-router-dom";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+axios.defaults.baseURL = "https://rentment-backend.onrender.com"
 
 export const AppContext = createContext();
 
